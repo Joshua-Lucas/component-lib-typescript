75fafffba48db25c7d86b23c88b74ddfb814c044
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-28 17:23:08 -0400

    Import StyleUtilites to help styling go faster

---
+++ src/StyleUtiles/ContainerSize.ts
@@ -0,0 +1,10 @@
+// (https://tailwindcss.com/docs/container)
+const screenSize = {
+  // Media Queries
+  sm: '(min-width: 640px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  xl: '(min-width: 1280px)'
+}
+
+export default screenSize
---
+++ src/StyleUtiles/index.ts
@@ -0,0 +1,7 @@
+import screenSize from './ContainerSize'
+import boxShadow from './BoxShadow'
+import borderRadius from './Borders'
+
+export default { screenSize, boxShadow, borderRadius }
+
+/* These are common and helpful css styles that i use often. Most of these are based off Tailwindcss Utility Styles (https://tailwindcss.com/). Which i would highly suggest if you are not useing style components */
