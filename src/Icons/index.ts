f8ddfa146dc74b6561f8a80bfc8d73c4f8f25c23
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-24 16:48:53 -0400

    Add new icon

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,5 +1,7 @@
 import ListBulleted from './list-bullet'
+import frontend from './frontend'
 
 export const Icons = {
-  BulletdList: ListBulleted //viewBox='0 0 20 20'
+  BulletdList: ListBulleted, //viewBox='0 0 20 20'
+  Frontend: frontend //viewBox='0 0 1200 1200'
 }
